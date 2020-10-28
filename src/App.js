9b31b0f4484c5b42e76429ed03a67ce1f7c41707
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-10-28 16:38:56 +0200

    - add another example of suspense

--- src/App.js
+++ src/App.js
@@ -1,48 +1,53 @@
-import React, { Component, Suspense } from 'react';
-import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+import React, {Component, Suspense} from 'react';
+import {BrowserRouter, Route, NavLink} from 'react-router-dom';
 import User from './containers/User';
 import Welcome from './containers/Welcome';
 /*
 import Posts from './containers/Posts';
 */
 
-const Posts = React.lazy(()=>import('./containers/Posts'));
+const Posts = React.lazy(() => import('./containers/Posts'));
 
 
 class App extends Component {
 
-  state= {
-    show:false
-  }
+    state = {
+        showPosts: false
+    }
 
-  modeHandler= () =>{
-    this.setState(prevState => {
-      return {show: !prevState};
-    })
-  }
+    modeHandler = () => {
+        this.setState(prevState => {
+            return {showPosts: !prevState.showPosts};
+        })
+    }
 
-  render() {
-    return (
-        <button >Toggle Mode</button>
+    render() {
+        return (
+            <React.Fragment>
+                <button onClick={this.modeHandler}>Toggle Mode</button>
+                {this.state.showPosts
+                    ? (<Suspense fallback={<div>Loading...</div>}> <Posts/> </Suspense>)
+                    : <User/>
+                }
+            </React.Fragment>
 
-
-     /* <BrowserRouter>
-        <React.Fragment>
-          <nav>
-            <NavLink to="/user">User Page</NavLink> |&nbsp;
-            <NavLink to="/posts">Posts Page</NavLink>
-          </nav>
-          <Route path="/" component={Welcome} exact />
-          <Route path="/user" component={User} />
-          <Route path="/posts" render={()=>
-              <Suspense fallback={<div>Loading...</div>}>
-                <Posts />
-              </Suspense>}
-          />
-        </React.Fragment>
-      </BrowserRouter>*/
-    );
-  }
+            /* <BrowserRouter>
+               <React.Fragment>
+                 <nav>
+                   <NavLink to="/user">User Page</NavLink> |&nbsp;
+                   <NavLink to="/posts">Posts Page</NavLink>
+                 </nav>
+                 <Route path="/" component={Welcome} exact />
+                 <Route path="/user" component={User} />
+                 <Route path="/posts" render={()=>
+                     <Suspense fallback={<div>Loading...</div>}>
+                       <Posts />
+                     </Suspense>}
+                 />
+               </React.Fragment>
+             </BrowserRouter>*/
+        );
+    }
 }
 
 export default App;
