a5e52c72fa675eac2a63ce3508db7ad74f9ba35d
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-10-28 16:27:59 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+import User from './containers/User';
+import Welcome from './containers/Welcome';
+/*
+import Posts from './containers/Posts';
+*/
+
+const Posts = React.lazy(()=>import('./containers/Posts'));
+
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <React.Fragment>
+          <nav>
+            <NavLink to="/user">User Page</NavLink> |&nbsp;
+            <NavLink to="/posts">Posts Page</NavLink>
+          </nav>
+          <Route path="/" component={Welcome} exact />
+          <Route path="/user" component={User} />
+          <Route path="/posts" component={Posts} />
+        </React.Fragment>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
